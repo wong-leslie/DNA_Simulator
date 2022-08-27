81b905b1640138cd9f07a37c8e4a8e6f173388a8
Leslie Wong <101053811+wong-leslie@users.noreply.github.com>
2022-08-27 17:27:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,12 +19,11 @@ const mockUpStrand = () => {
 // returns an object that contains properties (dna, and specimenNum)
 //specimen num should not be duplicated
 const pAeqourFactory = (specimenNum, dna) => {
-    if (specimenNum )
     return{
         dna: dna, 
         specimenNum: specimenNum, 
-
-        //return object's dna
+        
+        //return object's dna mutated
         mutate(){
             let randomSelectedIndex = Math.floor(Math.random() * this.dna.length)
             const randomBaseValue = this.dna[randomSelectedIndex]
@@ -40,6 +39,7 @@ const pAeqourFactory = (specimenNum, dna) => {
             
         }, 
 
+        //prints messafe that states at what percentage the two DNA objects are common/similar
         compareDNA(otherpAeqourObj){
             let count = 0;
             // let percentage = count/otherpAeqourObj.dna.length;
@@ -50,8 +50,42 @@ const pAeqourFactory = (specimenNum, dna) => {
             }
             const percent = Math.floor(count/(this.dna.length) * 100);
             console.log(`Specimen #${this.specimenNum} and Specimen #${otherpAeqourObj.specimenNum} have ${percent}% DNA in common.`)
+        }, 
+
+        //return true/false if objects DNA array contains at least 60% of C or G bases; determining DNAs survival
+        willLikelySruvive(){    
+            let count = 0;
+            for (let baseElement of this.dna){
+                if (baseElement === "G" || baseElement ==="C"){
+                    count+=1;
+                }
+            
+            }
+            const survivalRatio = count/this.dna.length;
+            // console.log(this.dna);
+            // console.log(count);
+            // console.log(survivalRatio)
+
+        
+            if (survivalRatio >= 0.60){
+                return true
+            }  return false;
         }
     }
 }
 
-console.log(pAeqourFactory(4, mockUpStrand()).compareDNA(pAeqourFactory(7, mockUpStrand())));
+
+
+// console.log(pAeqourFactory(4, mockUpStrand()).willLikelySruvive());
+
+
+let studyArray = []
+let randomInt = Math.floor(Math.random()*30)
+
+// while (studyArray.length < 30){
+//     if(pAeqourFactory(randomInt, mockUpStrand()).willLikelySruvive === true){
+//         studyArray.push(pAeqourFactory(randomInt, mockUpStrand()))
+//     }
+// }
+
+console.log(randomInt);
